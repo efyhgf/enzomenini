16d31ba6d85d257601a15dd94429cf2ebc3e071c
efyhgf <150448888+efyhgf@users.noreply.github.com>
2026-04-30 17:16:58 +0200

    Add files via upload

--- data.js
+++ data.js
@@ -1,11 +1,19 @@
 const PROJECT_DATA = {
   projet1: {
     c1: [
-      { src: "images/p1-c1-1.jpg", caption: "Gestion serveur" },
-      { src: "images/p1-c1-2.jpg", caption: "Base de données" }
+      { src: "images/C1/habilitations 1.png", caption: "Gestion des habilitations" },
+      { src: "images/C1/habilitations 2.png", caption: "Gestion des habilitations" }
     ],
-    c2: [
-      { src: "images/p1-c2-1.jpg", caption: "Support utilisateur" }
+    c3: [
+      { src: "images/C3/evolution 1.png", caption: "Évolutions"},
+      { src: "images/C3/evolution 2.png", caption: "Évolutions"},
+      { src: "images/C3/valorisation 1.png", caption: "Valorisation de l'organisation"}
+    ],
+    c4: [
+      { src: "images/C4/planifer 1.png", caption: "Planification des tâches"}
+    ],
+    c5: [
+      { src: "images/C5/accompagnement.png", caption: "Accompagnement des utilisateurs" }
     ]
   },
 
@@ -17,7 +25,25 @@ const PROJECT_DATA = {
 
   projet3: {
     c1: [
-      { src: "images/p3-c1-1.jpg", caption: "Site web BIOlinéa" }
+      { src: "images/C1/habilitations 3.png", caption: "Gestion des habilitations" }
+    ],
+    c3: [
+      { src: "images/C3/referencer 1.png", caption: "Référencer" }
+    ],
+    c5: [
+      { src: "images/C5/tests integrations 1.png", caption: "Tests d'intégration" }
+    ],
+    c6: [
+      { src: "images/C6/veille 3.png", caption: "Veille informationnelle" }
+    ]
+  },
+
+   projet4: {
+    c6: [
+      { src: "images/C6/env perso 1.png", caption: "Environnement de travail personnel" },
+      { src: "images/C6/identite pro 1.png", caption: "Identité professionnelle" },
+      { src: "images/C6/veille 1.png", caption: "Veille informationnelle" },
+      { src: "images/C6/veille 2.png", caption: "Veille informationnelle" }
     ]
   }
 };

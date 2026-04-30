4564a82a133dd0c8f3da36091a7102f91b19e04c
efyhgf <150448888+efyhgf@users.noreply.github.com>
2026-04-30 17:34:38 +0200

    Add files via upload

--- data.js
+++ data.js
@@ -1,19 +1,19 @@
 const PROJECT_DATA = {
   projet1: {
     c1: [
-      { src: "images/C1/habilitations 1.png", caption: "Gestion des habilitations" },
-      { src: "images/C1/habilitations 2.png", caption: "Gestion des habilitations" }
+      { src: "images/C1/habilitations_1.png", caption: "Gestion des habilitations" },
+      { src: "images/C1/habilitations_2.png", caption: "Gestion des habilitations" }
     ],
     c3: [
-      { src: "images/C3/evolution 1.png", caption: "Évolutions"},
-      { src: "images/C3/evolution 2.png", caption: "Évolutions"},
-      { src: "images/C3/valorisation 1.png", caption: "Valorisation de l'organisation"}
+      { src: "images/C3/evolution_1.png", caption: "Évolutions"},
+      { src: "images/C3/evolution_2.png", caption: "Évolutions"},
+      { src: "images/C3/valorisation_1.png", caption: "Valorisation de l'organisation"}
     ],
     c4: [
-      { src: "images/C4/planifer 1.png", caption: "Planification des tâches"}
+      { src: "images/C4/planifer_1.png", caption: "Planification des tâches"}
     ],
     c5: [
-      { src: "images/C5/accompagnement.png", caption: "Accompagnement des utilisateurs" }
+      { src: "images/C5/accompagnement_1.png", caption: "Accompagnement des utilisateurs" }
     ]
   },
 
@@ -25,25 +25,25 @@ const PROJECT_DATA = {
 
   projet3: {
     c1: [
-      { src: "images/C1/habilitations 3.png", caption: "Gestion des habilitations" }
+      { src: "images/C1/habilitations_3.png", caption: "Gestion des habilitations" }
     ],
     c3: [
-      { src: "images/C3/referencer 1.png", caption: "Référencer" }
+      { src: "images/C3/referencer_1.png", caption: "Référencer" }
     ],
     c5: [
-      { src: "images/C5/tests integrations 1.png", caption: "Tests d'intégration" }
+      { src: "images/C5/tests_integrations_1.png", caption: "Tests d'intégration" }
     ],
     c6: [
-      { src: "images/C6/veille 3.png", caption: "Veille informationnelle" }
+      { src: "images/C6/veille_3.png", caption: "Veille informationnelle" }
     ]
   },
 
    projet4: {
     c6: [
-      { src: "images/C6/env perso 1.png", caption: "Environnement de travail personnel" },
-      { src: "images/C6/identite pro 1.png", caption: "Identité professionnelle" },
-      { src: "images/C6/veille 1.png", caption: "Veille informationnelle" },
-      { src: "images/C6/veille 2.png", caption: "Veille informationnelle" }
+      { src: "images/C6/env_perso_1.png", caption: "Environnement de travail personnel" },
+      { src: "images/C6/identite_pro_1.png", caption: "Identité professionnelle" },
+      { src: "images/C6/veille_1.png", caption: "Veille informationnelle" },
+      { src: "images/C6/veille_2.png", caption: "Veille informationnelle" }
     ]
   }
 };

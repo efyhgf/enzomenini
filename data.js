9d300ec147bff20de350ee13d22a91877c064926
efyhgf <150448888+efyhgf@users.noreply.github.com>
2026-05-01 16:31:07 +0200

    Add files via upload

--- data.js
+++ data.js
@@ -7,10 +7,10 @@ const PROJECT_DATA = {
     c3: [
       { src: "images/C3/evolution_1.png", caption: "Évolutions"},
       { src: "images/C3/evolution_2.png", caption: "Évolutions"},
-      { src: "images/C3/valorisation_1.png", caption: "Valorisation de l'organisation"}
+      { src: "images/C3/valorisation_1.PNG", caption: "Valorisation de l'organisation"}
     ],
     c4: [
-      { src: "images/C4/planifer_1.png", caption: "Planification des tâches"}
+      { src: "images/C4/planifer_1.PNG", caption: "Planification des tâches"}
     ],
     c5: [
       { src: "images/C5/accompagnement_1.png", caption: "Accompagnement des utilisateurs" }
@@ -25,25 +25,25 @@ const PROJECT_DATA = {
 
   projet3: {
     c1: [
-      { src: "images/C1/habilitations_3.png", caption: "Gestion des habilitations" }
+      { src: "images/C1/habilitations_3.PNG", caption: "Gestion des habilitations" }
     ],
     c3: [
       { src: "images/C3/referencer_1.png", caption: "Référencer" }
     ],
     c5: [
-      { src: "images/C5/tests_integrations_1.png", caption: "Tests d'intégration" }
+      { src: "images/C5/tests_integrations_1.PNG", caption: "Tests d'intégration" }
     ],
     c6: [
-      { src: "images/C6/veille_3.png", caption: "Veille informationnelle" }
+      { src: "images/C6/veille_3.PNG", caption: "Veille informationnelle" }
     ]
   },
 
    projet4: {
     c6: [
       { src: "images/C6/env_perso_1.png", caption: "Environnement de travail personnel" },
-      { src: "images/C6/identite_pro_1.png", caption: "Identité professionnelle" },
-      { src: "images/C6/veille_1.png", caption: "Veille informationnelle" },
-      { src: "images/C6/veille_2.png", caption: "Veille informationnelle" }
+      { src: "images/C6/identite_pro_1.PNG", caption: "Identité professionnelle" },
+      { src: "images/C6/veille_1.PNG", caption: "Veille informationnelle" },
+      { src: "images/C6/veille_2.PNG", caption: "Veille informationnelle" }
     ]
   }
 };
